79c1153bed4292112e3cf0a5bf8ac71c4c2cba8a
Lima Junior <junior1842s@gmail.com>
2022-06-19 11:39:02 -0300

    commit inicial

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+namespace OlaMundo
+{
+    public class Hello
+    {
+        public static void Main(string[] i)
+        {
+            string Ola = "Hello World!";
+            Console.WriteLine(Ola);
+        }
+    }
+}
